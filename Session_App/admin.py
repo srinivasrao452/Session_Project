86a58b72fa9a731cfc360a7d7839642f9f20f446
srinivasrao <srinivasrao452@gmail.com>
2021-11-08 11:49:47 +0530

    Admin Update

--- Session_App/admin.py
+++ Session_App/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 
 # Import  required model here
 # register our required modal name with admin site
+
+# if you want user defined custom logics then we can create Userdefined Admin class
